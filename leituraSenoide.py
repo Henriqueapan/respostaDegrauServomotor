ec1f0a63a93a4e85bfca6cf5512ed95a9044e7f6
henrique.pancotti <henrique.pancotti@hotmail.com>
2024-05-24 18:36:54 -0300

    Adaptando geracao de senoide pra alimentar motor

--- leituraSenoide.py
+++ leituraSenoide.py
@@ -1,22 +1,33 @@
 import serial, datetime, time
 
 ser = serial.Serial(
-    port="COM7",
+    port="COM5",
     baudrate=115200,
     timeout=0
 )
 
 time.sleep(1.5)
 
+# Buffer de entrada para cálculo de média móvel
+M = 50
+inv_M = 1/M
+buffer = [0] * M
+soma_buffer = sum(buffer)
+
 # Lists tempo, velocidade e input de senóide
 tempo = list()
 sin_input = list()
 
 # Tempo de execução em segundos
-tempo_execucao = 5
+tempo_execucao = 12
 
 # Variável de tempo atual
 tempo_atual = 0
+delta_tempo = 0
+
+# Valor máximo a ser considerado como ruído e não sinal
+MAX_NOISE_VAL = 120
+TUNING_CONSTANT = 100
 
 print("Conectando a porta: " + ser.portstr)
 
@@ -27,10 +38,16 @@ while True:
     if ser.in_waiting <= 0:
         continue
     enc_output = ser.readline().decode("ascii").rstrip("\n").rstrip("\r").rstrip().split(", ")
+    if len(enc_output) != 2 and delta_tempo is not None:
+        tempo_atual += delta_tempo
+        continue
+    elif len(enc_output) != 2: continue
 
     try:
-        sin_input.append(float(enc_output[0]))
-        delta_tempo = float(enc_output[1])/1000
+        # buffer[i%M] = float(enc_output[0]) if float(enc_output[0]) > MAX_NOISE_VAL else buffer[i%M - 1]
+        # buffer[i%M] = float(enc_output[0])
+        sin_input.append(float(enc_output[0]) if float(enc_output[0]) > MAX_NOISE_VAL else sin_input[-1])
+        delta_tempo = float(enc_output[1])/1e6
     except ValueError as e:
         continue
 
@@ -38,11 +55,12 @@ while True:
     tempo.append(tempo_atual)
 
     if(tempo[i] > tempo_execucao): break
+    else: print(tempo[i])
 
     i += 1
 
 with open(f'./output/output{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt', 'w') as file:
-    file.writelines([f'{str(velocidade)},{str(tempo)},{str(sin_input)}\n' for velocidade, tempo, sin_input in zip(velocidade, tempo, sin_input)])
+    file.writelines([f'{str(tempo)},{str(sin_input)}\n' for tempo, sin_input in zip(tempo, sin_input)])
     file.close()
 
 ser.close()
--- respostaEmFrequenciaTempoReal/respostaEmFrequenciaTempoReal.ino
+++ respostaEmFrequenciaTempoReal/respostaEmFrequenciaTempoReal.ino
@@ -1,5 +1,5 @@
 //#define PWM_PIN 9  // Define o pino PWM a ser usado
-#define PWM_freq 100 // Frequência em Hertz
+#define PWM_FREQ 0.1 // Frequência em Hertz
 #define AMPLITUDE 127  // Amplitude máxima do sinal PWM (0 a 255)
 #define MOTOR_PIN_1 7 // Pino do motor (IN1)
 #define MOTOR_PIN_2 6 // Pino do motor (IN2)
@@ -10,16 +10,15 @@
 volatile int chA_antigo = 0;
 volatile int chB_antigo = 0;
 volatile int contador = 0;
-volatile float valor_pwm;
-double tempoAtual = 0;
-double tempoAnterior = 0;
-// double PWM_PERIOD = ((1/PWM_freq)*1000)/360; // Periodo em millissegundos
+volatile float valor_pwm = 0;
+volatile double tempoAtual = 0;
+volatile double tempoAnterior = 0;
+volatile double arg;
 double radianos;
-float inv_mili = 1/1000;
+float inv_micro = 1/1000000;
 
 void setup() {
   // Configuração do pino PWM
-  // pinMode(PWM_PIN, OUTPUT);
   pinMode(MOTOR_PIN_1, OUTPUT);
   pinMode(MOTOR_PIN_2, OUTPUT);
   pinMode(MOTOR_ENABLE, OUTPUT);
@@ -29,29 +28,30 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(chB), leituraEncoder, CHANGE);
   
   // Configuração da frequência PWM
-  TCCR1B = (TCCR1B & 0xF8) | 0x01; // Configura a frequência PWM para 31.37kHz
   Serial.begin(115200);
 }
 
 void loop() {
-  //tempoAtual =millis();
-  if (tempoAtual <=10000){  // if Para acionar o motor por apenas 2500 millissegundos
-    tempoAtual = millis();
-    valor_pwm = AMPLITUDE/2*(sin(2*PI*PWM_freq*tempoAtual))+AMPLITUDE*1.5;
-    
+  if (tempoAtual <= 15*1000000) {
+    tempoAtual = micros();
+
+    arg = TWO_PI * PWM_FREQ /*Hz*/ * double(tempoAtual)/(1000000);//*0.0000000001;
+
+    valor_pwm = ((127/2)*sin(arg)) + 1.5*127;
+
     // Escrevendo o valor PWM no pino
     controlaMotor(0,1,valor_pwm);
-    //Serial.println(valor_pwm, 10);
-    Serial.println(String(contador) + ", " + String(tempoAtual-tempoAnterior) + ", "+ String(valor_pwm));
 
+    Serial.println(String(valor_pwm) + ", " + String((tempoAtual - tempoAnterior)) + ", "+ String(valor_pwm));
+    
     contador = 0; // Reinicia o contador
-    tempoAnterior = tempoAtual; // Atualiza o tempo
+    tempoAnterior = tempoAtual;
   }
   else{ // Desliga o motor e para de enviar
     controlaMotor(0,0,0); 
     Serial.end();
   }
-  delay(1);
+  delay(10);
 }
 
 void controlaMotor (bool in1, bool in2, float pwm){
