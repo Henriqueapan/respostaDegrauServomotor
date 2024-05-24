e2646826989bcd76017d0f47b0fa71a93b680581
henrique.pancotti <henrique.pancotti@hotmail.com>
2024-05-24 18:29:41 -0300

    Código arduino para geração de senoide real

---
+++ senoideReal/senoideReal.ino
@@ -0,0 +1,26 @@
+#define PWM_FREQ 0.1
+
+volatile double tempoAtual = 0.0;
+volatile double tempoAnterior = 0.0;
+volatile double valor_pwm;
+volatile double arg;
+float tuned_freq = PWM_FREQ;
+int tuning_constant = 1;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (tempoAtual <= 15*1000000) {
+    tempoAtual = micros();
+
+    arg = TWO_PI * tuned_freq /*Hz*/ * double(tempoAtual)/(1000000);//*0.0000000001;
+
+    valor_pwm = ((127/2)*sin(arg)) + 1.5*127;
+    
+    Serial.println(String(valor_pwm) + ", " + String((tempoAtual - tempoAnterior)));
+    
+    tempoAnterior = tempoAtual;
+  }
+}
