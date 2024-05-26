3b23d4daa34d759c147505de1c4bd1fbe0a9dfe4
henrique.pancotti <henrique.pancotti@engenharia.ufjf.br>
2024-05-26 15:54:40 -0300

    Limpeza no código de respostaDegrauV3 e diretório com PDF do relatório

--- respostaDegrauV3/respostaDegrauV3.ino
+++ respostaDegrauV3/respostaDegrauV3.ino
@@ -4,20 +4,13 @@
 #define MOTOR_PIN_2 6 // Pino do motor (IN2)
 #define MOTOR_ENABLE 5 // Pino do enable da ponte H
 
-#define EncResolution 100
-#define pi 3.14159
-#define N 8
-
-volatile int contador = 0;
-unsigned long tempoAtual;
-unsigned long tempoAnterior = 0;
+volatile int contador_passos_motor = 0;
+unsigned long tempo_atual;
+unsigned long tempo_anterior = 0;
 int interrupcao_atual = 0;
 int chA_antigo = 0;
 int chB_antigo = 0;
 float velocidade = 0;
-float soma_contador = 0;
-float media_contador;
-float inv_N = 1/N;
 
 void setup() {
   pinMode(chA, INPUT);
@@ -33,16 +26,17 @@ void setup() {
 }
 
 void loop() {
-  tempoAtual = millis(); // atualiza o quanto tempo se passou desde o início
+  tempo_atual = millis(); // Atualiza o quanto tempo se passou desde o início
 
-  if (tempoAtual <=15000){  // if Para acionar o motor por apenas 2500 millissegundos
+  if (tempo_atual <= 15000){  // Condicional para acionar o motor por apenas 15000 millissegundos
 
-    controlaMotor(0,1,255); // Fazendo o motor girar em alguma direção na velocidade 255
+    controlaMotor(0,1,255); // Fazendo o motor girar no sentido horário na velocidade 255
 
-    Serial.println(String(contador) + "," + String(tempoAtual-tempoAnterior));
+    // Imprimindo na saída serial os dados de 
+    Serial.println(String(contador_passos_motor) + "," + String(tempo_atual-tempo_anterior));
     
-    contador = 0; // Reinicia o contador
-    tempoAnterior = tempoAtual; // Atualiza o tempo
+    contador_passos_motor = 0; // Reinicia o contador_passos_motor
+    tempo_anterior = tempo_atual; // Atualiza o tempo
   }
   else{ // Desliga o motor e para de enviar
     controlaMotor(0,0,0); 
@@ -57,27 +51,27 @@ void leituraEncoder() {
   int chB_atual = digitalRead(chB);
 
   if (chA_antigo == 0 && chB_antigo == 0) {
-    if(chA_atual == 1 && chB_atual == 1) contador = contador + 2;
-    else if(chA_atual == 1 && chB_atual == 0) contador--;
-    else if(chA_atual == 0 && chB_atual == 1) contador++;
+    if(chA_atual == 1 && chB_atual == 1) contador_passos_motor = contador_passos_motor + 2;
+    else if(chA_atual == 1 && chB_atual == 0) contador_passos_motor--;
+    else if(chA_atual == 0 && chB_atual == 1) contador_passos_motor++;
   }
   else if (chA_antigo == 0 && chB_antigo == 1) {
-    if(chA_atual == 1 && chB_atual == 1) contador ++;
-    else if(chA_atual == 1 && chB_atual == 0) contador = contador - 2;
-    else if(chA_atual == 0 && chB_atual == 1) contador = contador;
-    else contador--;
+    if(chA_atual == 1 && chB_atual == 1) contador_passos_motor ++;
+    else if(chA_atual == 1 && chB_atual == 0) contador_passos_motor = contador_passos_motor - 2;
+    else if(chA_atual == 0 && chB_atual == 1) contador_passos_motor = contador_passos_motor;
+    else contador_passos_motor--;
   }
   else if (chA_antigo == 1 && chB_antigo == 0) {
-    if(chA_atual == 1 && chB_atual == 1) contador--;
-    else if(chA_atual == 1 && chB_atual == 0) contador = contador;
-    else if(chA_atual == 0 && chB_atual == 1) contador = contador - 2;
-    else contador ++;
+    if(chA_atual == 1 && chB_atual == 1) contador_passos_motor--;
+    else if(chA_atual == 1 && chB_atual == 0) contador_passos_motor = contador_passos_motor;
+    else if(chA_atual == 0 && chB_atual == 1) contador_passos_motor = contador_passos_motor - 2;
+    else contador_passos_motor ++;
   }
   else if (chA_antigo = 1 && chB_antigo == 1) {
-    if(chA_atual == 1 && chB_atual == 1) contador = contador;
-    else if(chA_atual == 1 && chB_atual == 0) contador++;
-    else if(chA_atual == 0 && chB_atual == 1) contador--;
-    else contador = contador + 2;
+    if(chA_atual == 1 && chB_atual == 1) contador_passos_motor = contador_passos_motor;
+    else if(chA_atual == 1 && chB_atual == 0) contador_passos_motor++;
+    else if(chA_atual == 0 && chB_atual == 1) contador_passos_motor--;
+    else contador_passos_motor = contador_passos_motor + 2;
   }
 
   chA_antigo = chA_atual;
