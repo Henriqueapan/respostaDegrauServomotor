70bcef4e63eecc74ca56a12045840d0cec3d3ec2
Mateus <mateusfeliciano77@gmail.com>
2024-05-26 18:00:07 -0300

    Pastas com script do arduino utilizando otimizações do chatgpt  e lib encoder.h

--- plotOutputPython.m
+++ plotOutputPython.m
@@ -14,14 +14,14 @@ for file = files'
   i++
   subplot(2,1,1);
   plot(tempo, vel)#/10.8
- # xlim([0 2]);
+  xlim([0 2]);
   xlabel("Tempo (s)");
   ylabel("Velocidade (rad/s)")
   #figure(i)
   #i++
   subplot(2,1,2);
   plot(tempo, sin)
-  #xlim([0 2]);
+  xlim([0 2]);
   xlabel("Tempo(s)");
   ylabel("Tensão (V)");
 endfor
