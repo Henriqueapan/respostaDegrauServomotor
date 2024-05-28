10dc9964aeb2a1e63ab8726e65650dd1bffd2445
Mateus <mateusfeliciano77@gmail.com>
2024-05-28 12:04:06 -0300

    Aumento da espessura da linha do diagrama de bode experimental e tentativa de plotar o diagrama experimental e o teórico (continuar mais tarde)

--- bodeSobrepostos.m
+++ bodeSobrepostos.m
@@ -1,12 +1,18 @@
+pkg load control;
 M = [3.7 4.87 6.55 7.9 11.14 12.21 15.138 18.34 21.60 23.52 25.51 23.36 25.2 25.7 24.97 26.00];
 freqs = [25 17.5 10 7.5 6 5 3.5 2.5 1.5 1 0.75 0.5 0.35 0.25 0.175 0.1] * 2 * pi;
 dt = [0.0210 0.030 0.05 0.054 0.062 0.0579 0.08 0.0740 0.082 0.078 0.06 0.09 0.08 0.06 0.077 0.009]/(2*pi);
 freqsReais = [17.25 12.19 6.91 5.24 3.87 3.48 2.247 1.74 1.02 0.707 0.529 0.3544 0.227 0.1762 0.1137 0.07 ]*(2*pi);
 G = 20*log10(M);
 fase =(dt.*freqsReais)*360;
+Gfreq = (11504.73)/(s^2 + 43.37*s + 470.24);
+[mag, phase, w] = bode(G);
 
+figure()
 subplot(2,1,1);
 semilogx(freqsReais, G);
+mag = squeeze(mag);
+semilogx(w,mag);
 % xlabel("Frequência (rad/s)");
 ylabel("Magnitude (dB)");
 grid on
@@ -17,6 +23,8 @@ set(h(1), 'LineWidth', 2);
 
 subplot(2,1,2);
 semilogx(freqsReais,-fase);
+phase = squeeze(phase);
+semilogx(w,phase);
 xlabel("Frequência (rad/s)");
 ylabel("Fase (graus)");
 grid on
@@ -26,13 +34,3 @@ h = get(gca, 'Children');
 set(h(1), 'Color', 'b');
 set(h(1), 'LineWidth', 2);
 
-
-##g = tf([44.85],[0.075589 1])
-##g2 = tf([868.2410319],[1 1736.482064 868.2410319])
-#g3 = tf([14.71444775*14.71444775],[1 2*1*14.71444775 14.71444775*14.71444775])
-#g4 = zpk([],[-6.516 -1.434], 420.33)
-
-27,7711526
-
-24,46569909
-##g2 = tf([21.685^2],[1 2*21.685 21.685^2])
