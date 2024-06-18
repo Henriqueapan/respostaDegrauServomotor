c027d5ed0ccc5d8c7161d44130876416f8ea11d2
henrique.pancotti <henrique.pancotti@engenharia.ufjf.br>
2024-06-17 21:02:34 -0300

    Tentativa de média móvel no arduino

--- servoWithPID/servoWithPID.ino
+++ servoWithPID/servoWithPID.ino
@@ -1,18 +1,40 @@
+#include <TimerOne.h>
+
 #define chA 3 // Pino canal A do encoder
 #define chB 2 // Pino canal B do encoder
 #define MOTOR_PIN_1 7 // Pino do motor (IN1)
 #define MOTOR_PIN_2 6 // Pino do motor (IN2)
 #define MOTOR_ENABLE 5 // Pino do enable da ponte H
+#define JANELA_MEDIA_MOVEL 50
+#define RESOLUCAO_ENCODER 200 // Resolução do encoder (quantidaed de passos que representa 1 volta completa)
+#define INV_MICRO 1/1000000
+#define PERIODO_AMOSTRAGEM 100 // Microssegundos
 
+float INV_PERIODO_AMOSTRAGEM = 1/PERIODO_AMOSTRAGEM;
+float INV_JANELA_MEDIA_MOVEL = 1/JANELA_MEDIA_MOVEL;
+float INV_RESOLUCAO_ENCODER = 1/RESOLUCAO_ENCODER;
 volatile int contador_passos_motor = 0;
+volatile int contador_passos_motor_anterior = 0;
 unsigned long tempo_atual;
 unsigned long tempo_anterior = 0;
 int interrupcao_atual = 0;
 int chA_antigo = 0;
 int chB_antigo = 0;
-float velocidade = 0;
+volatile double delta_tempo;
+volatile double arr_velocidade[JANELA_MEDIA_MOVEL];
+volatile double velocidade = 0;
+int contador_pos_vetor_velocidade = 0;
+bool motor_ligado = false;
 
 void setup() {
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(calculaERegistraVel);
+
+  // Inicialiando vetor de velocidade para média móvel
+  for(int j = 0; j < JANELA_MEDIA_MOVEL; j++) {
+    arr_velocidade[j] = 0;
+  }
+
   pinMode(chA, INPUT);
   pinMode(chB, INPUT);
   pinMode(MOTOR_PIN_1, OUTPUT);
@@ -26,17 +48,30 @@ void setup() {
 }
 
 void loop() {
-  tempo_atual = millis(); // Atualiza o quanto tempo se passou desde o início
+  // Não deve ser necessário obter o tempo atual no loop. Isso é feito no interrupt do Timer1
+  tempo_atual = micros(); // Atualiza o quanto tempo se passou desde o início
 
-  if (tempo_atual <= 15000){  // Condicional para acionar o motor por apenas 15000 millissegundos
+  if (tempo_atual <= 3 * 1000000){  // Condicional para acionar o motor por apenas 15 segundos
 
     controlaMotor(0,1,255); // Fazendo o motor girar no sentido horário na velocidade 255
 
-    // Imprimindo na saída serial os dados de quantidades de passos do eixo do motor e intervalo de tempo referente a esses passos
-    Serial.println(String(contador_passos_motor) + "," + String(tempo_atual-tempo_anterior));
+    // Velocidade calculada por média móvel
+    for (int i = 0; i < JANELA_MEDIA_MOVEL; i++) {
+      // Serial.println(String(arr_velocidade[i]));
+      velocidade += abs(arr_velocidade[i]);
+      Serial.println(String(abs(arr_velocidade[i])));
+      // Serial.println(String(arr_velocidade[i]));
+    }
+    // Serial.println(String(velocidade, 7));
+    // Serial.println("Fim");
+    velocidade = velocidade * INV_JANELA_MEDIA_MOVEL;
     
-    contador_passos_motor = 0; // Reinicia o contador de passos do eixo do motor
-    tempo_anterior = tempo_atual; // Define a variável de controle para cálculo de variação de tempo tempo_anterior como o tempo atual
+    // Imprime na saída serial a situação atual do vetor velocidade
+    Serial.println(String(velocidade, 15));
+
+    velocidade = 0;
+
+    // Aplica a velocidade (obtida da média móvel no vetor double velocidade) a entrada com PID
   }
   else{ // Desliga o motor e para de enviar dados
     controlaMotor(0,0,0); 
@@ -77,6 +112,25 @@ void leituraEncoder() {
   chA_antigo = chA_atual;
   chB_antigo = chB_atual;
 
+}
+
+void calculaERegistraVel(void) {
+  // Serial.println("Reached");
+  if (contador_pos_vetor_velocidade == 1000) contador_pos_vetor_velocidade = 0;
+
+  // TODO: experimentar usar a frequencia definida do Timer1 ao inves do delta_tempo
+  double mov_ang = contador_passos_motor * INV_RESOLUCAO_ENCODER * 2 * PI;
+
+  // tempo_atual = micros();
+  // delta_tempo = (tempo_atual - tempo_anterior) * INV_MICRO;
+
+  int idx_velocidade = (int)contador_pos_vetor_velocidade % JANELA_MEDIA_MOVEL;
+  // Serial.println(String((double)PERIODO_AMOSTRAGEM*INV_MICRO, 10));
+  arr_velocidade[idx_velocidade] = mov_ang * ((double)PERIODO_AMOSTRAGEM * INV_MICRO);//(delta_tempo);
+
+  // tempo_anterior = tempo_atual;
+  contador_passos_motor = 0;
+  contador_pos_vetor_velocidade++;
 }
 
 void controlaMotor(bool in1, bool in2, float pwm) {
