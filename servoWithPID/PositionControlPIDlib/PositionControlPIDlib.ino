c28f3763fb945906d2f97a9282b1335afdbd2bc6
Mateus <mateusfeliciano77@gmail.com>
2024-07-13 01:05:20 -0300

    indiano

--- servoWithPID/PositionControlPIDlib/PositionControlPIDlib.ino
+++ servoWithPID/PositionControlPIDlib/PositionControlPIDlib.ino
@@ -1,5 +1,6 @@
 #include <TimerOne.h>
 #include <Encoder.h>
+#include <PID_v1.h>
 
 #define chA 2 // Pino canal A do encoder
 #define chB 3 // Pino canal B do encoder
@@ -8,17 +9,11 @@
 #define MOTOR_PIN_2 6 // Pino do motor (IN2)
 #define MOTOR_ENABLE 5 // Pino do enable da ponte H
 #define RESOLUCAO_ENCODER 200 // Resolução do encoder (quantidade de passos que representa 1 volta completa)
-#define PERIODO_AMOSTRAGEM 1000 // Microssegundos
+#define PERIODO_AMOSTRAGEM 500 // Microssegundos
 #define INV_MICRO .000001
 
 #define PWM_MIN 60 // Valor mínimo de PWM para acionar o motor
 #define DEAD_ZONE 10 
-
-#define WINDOW_SIZE 25 // Tamanho da janela para a média móvel
-
-volatile double velocidadeBuffer[WINDOW_SIZE] = {0};
-volatile int bufferIndex = 0;
-volatile double somaVelocidade = 0;
 
 volatile double velocidade = 0;
 double INV_RESOLUCAO = 1.0/(double)RESOLUCAO_ENCODER;
@@ -26,7 +21,7 @@ double INV_AMOSTRAGEM = 1.0/(double)PERIODO_AMOSTRAGEM;
 volatile double erro = 0;
 volatile double ref = 0;
 volatile double saida_controle = 0;
-double K = 0.05;
+double K = 0.07;
 
 double COEF_EQ_DIFERENCAS_POSICAO = (PERIODO_AMOSTRAGEM/2) * INV_MICRO;
 volatile double velocidade_anterior = 0;
@@ -44,7 +39,13 @@ volatile int chB_antigo = 0;
 
 volatile int pwm_val = 0;
 
-Encoder myEnc(chA, chB);
+unsigned long tempo = 0;
+
+double kp = 0.024075 , ki = 0.00852 , kd = 0.01701;            // modify for optimal performance
+double input = 0, output = 0, setpoint = 0;
+PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);  
+
+// Encoder myEnc(chA, chB);
 
 void setup() {
   Timer1.initialize(PERIODO_AMOSTRAGEM);
@@ -57,7 +58,12 @@ void setup() {
   pinMode(MOTOR_PIN_2, OUTPUT);
   pinMode(MOTOR_ENABLE, OUTPUT);
   
-  TCCR1B = TCCR1B & 0b11111000 | 1;
+//   TCCR1B = TCCR1B & 0b11111000 | 1;
+
+  myPID.SetMode(AUTOMATIC);   //set PID in Auto mode
+  myPID.SetSampleTime(1);  // refresh rate of PID controller
+  myPID.SetOutputLimits(-254, 254); // this is the MAX PWM value to move motor, here change in value reflect change in speed of motor.
+
 //   attachInterrupt(digitalPinToInterrupt(chA), leituraEncoder, RISING);
   Serial.begin(115200);
 }
@@ -65,18 +71,21 @@ void setup() {
 void loop() {
     // Serial.println(String(contador_passos));
     // Serial.println("ref: " + String(ref) + " / " + "erro: " +  String(erro) + " / " + "vel: " +  String(velocidade) + " / " + "PWM: " + String(saida_controle));
-    Serial.println(String(ref) + " / " + String(erro) + " / " + String(velocidade) + " / " + String(pwm_val)+ "/ " + String(saida_controle)); 
+    Serial.println(String(ref) + " / " + String(erro) + " / " + String(velocidade) + " / " + String(pwm_val)+ "/ " + String(saida_controle));
+    delay(1); 
 }
 
 void interrupcao(){
-    // leituraEncoder();
-    leituraEncoder2();
+    // tempo = micros();
+    leituraEncoder();
+    // leituraEncoder2();
     calculaVelocidade();
-    integrador(mediaMovelVelocidade(velocidade));
+    integrador(velocidade);
     controladorPOS();
     // atualizarPWM();
     atualizarPWM2();
     atualizarMemorias();
+    // Serial.println(micros() - tempo);
 }
 
 void atualizarMemorias(){
@@ -87,7 +96,7 @@ void atualizarMemorias(){
     erro_anterior = erro;
     // erro_ante_anterior = erro_anterior;
     contador_passos_motor = 0;
-    myEnc.write(0);
+    // myEnc.write(0);
 }
 
 void atualizarPWM(){
@@ -103,16 +112,14 @@ void atualizarPWM2(){
     pwm_val = abs(saida_controle)*255;
 
     if (pwm_val < DEAD_ZONE) {
-        // Se o valor do PWM estiver dentro da zona morta, desligue o motor
         digitalWrite(MOTOR_PIN_1, LOW);
         digitalWrite(MOTOR_PIN_2, LOW);
         analogWrite(MOTOR_ENABLE, 0);
     } else {
-        // Garanta que o valor do PWM seja pelo menos PWM_MIN
         if (pwm_val < PWM_MIN) {
             pwm_val = PWM_MIN;
         }
-        pwm_val = constrain(pwm_val,0,255);
+        pwm_val = constrain(pwm_val,0,254);
         if (saida_controle < 0){
             controlaMotor(1, 0, pwm_val);
         }
@@ -125,11 +132,15 @@ void atualizarPWM2(){
 
 void controladorPOS(){
     ref = mapFloat(analogRead(REFERENCIA_PIN), 0, 1020, 0, TWO_PI);
+    setpoint = ref;
     erro = ref - posicao;
+    input = erro;
+    myPID.Compute();
     // saida_controle =  erro * K * 255 ;  
     // saida_controle = 0.567* K * PERIODO_AMOSTRAGEM * erro + 0.433 * K * PERIODO_AMOSTRAGEM * erro_anterior - saida_controle_anterior;
     // saida_controle = (0.24237 * (1 + 0.134 * INV_AMOSTRAGEM) * posicao + 0.24237 * (1 - 0.134 * INV_AMOSTRAGEM) * posicao_anterior) - saida_controle_anterior;
-    saida_controle = K * ((1 + 0.22 * INV_AMOSTRAGEM) * erro + (1 - 0.22 * INV_AMOSTRAGEM) * erro_anterior) - saida_controle_anterior;
+    // saida_controle = K * ((1 + 0.22 * INV_AMOSTRAGEM) * erro + (1 - 0.22 * INV_AMOSTRAGEM) * erro_anterior) - saida_controle_anterior;
+    saida_controle = output;
 }
 
 void calculaVelocidade(){
@@ -177,7 +188,7 @@ void leituraEncoder() {
 }
 
 void leituraEncoder2() {
-    contador_passos_motor = myEnc.read();
+    // contador_passos_motor = myEnc.read();
 }
 
 void controlaMotor(bool in1, bool in2, int valor_pwm){
@@ -188,22 +199,4 @@ void controlaMotor(bool in1, bool in2, int valor_pwm){
 
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-// Função para calcular a média móvel da velocidade
-double mediaMovelVelocidade(double novaVelocidade) {
-    // Subtrai o valor mais antigo da soma
-    somaVelocidade -= velocidadeBuffer[bufferIndex];
-
-    // Substitui o valor mais antigo pelo novo valor
-    velocidadeBuffer[bufferIndex] = novaVelocidade;
-
-    // Adiciona o novo valor à soma
-    somaVelocidade += novaVelocidade;
-
-    // Atualiza o índice do buffer, garantindo que ele volte ao início quando atingir o tamanho da janela
-    bufferIndex = (bufferIndex + 1) % WINDOW_SIZE;
-
-    // Calcula a média móvel
-    return somaVelocidade / WINDOW_SIZE;
 }
