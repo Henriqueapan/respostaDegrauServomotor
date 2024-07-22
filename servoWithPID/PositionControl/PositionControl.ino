c46bd3d0a14e7d53708e554f3e47aeadd46fc8c3
Mateus <mateusfeliciano77@gmail.com>
2024-07-22 00:20:30 -0300

    Muita coisa mudou, graficos, funções e tudo que há de mal

--- servoWithPID/PositionControl/PositionControl.ino
+++ servoWithPID/PositionControl/PositionControl.ino
@@ -9,34 +9,28 @@
 #define MOTOR_ENABLE 5 // Pino do enable da ponte H
 #define RESOLUCAO_ENCODER 200 // Resolução do encoder (quantidade de passos que representa 1 volta completa)
 #define PERIODO_AMOSTRAGEM 10000 // Microssegundos
-#define PERIODO_LEITURA_REFERENCIA 1000000 // Microssegundos
 #define INV_MICRO .000001
 
+#define PERIODO_LEITURA_REFERENCIA 1000000 // Microssegundos
+// #define PERIODO_LEITURA_REFERENCIA 0 // Microssegundos
+
+// Definições das limitações do motor
 #define PWM_MIN 70 // Valor mínimo de PWM para acionar o motor
 #define DEAD_ZONE 13 
 
-#define WINDOW_SIZE 25 // Tamanho da janela para a média móvel
-
-// Defina Variáveis
-double Setpoint, Input, Output;
-// Parâmetros PID
-double Kp = 0.039365, Ki = 0.015513, Kd = 0.0074816; // Considerando Gzoh
-// double Kp = 0.047273, Ki = 0.021656, Kd = 0.0098097; // desconsiderando Gzoh
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-
-volatile double buffer[WINDOW_SIZE] = {0};
-volatile int bufferIndex = 0;
-volatile double somaMedia = 0;
-
+// Tempo
 volatile unsigned long tempo_atual = 0;
 volatile unsigned long tempo_anterior = 0;
 
-volatile double velocidade = 0;
+// Constantes de calculo
 double PERIODO_AMOSTRAGEM_SEC = PERIODO_AMOSTRAGEM * INV_MICRO;
 double INV_RESOLUCAO = 1.0/(double)RESOLUCAO_ENCODER;
 double INV_AMOSTRAGEM = 1.0/(double)PERIODO_AMOSTRAGEM;
 double INV_AMOSTRAGEM_SEC = 1.0/(double)PERIODO_AMOSTRAGEM_SEC;
 double INV_LEITURA = 1.0/1020.0;
+
+// Variaveis de controle
+volatile double velocidade = 0;
 
 volatile double erro = 0;
 volatile double ref = 0;
@@ -51,8 +45,7 @@ volatile double posicao_anterior = 0;
 volatile double saida_controle_anterior = 0;
 volatile double saida_controle_ante_anterior = 0;
 
-volatile double in_anterior = 0;
-
+// Variáveis da leitura do encoder
 volatile int contador_passos_motor = 0;
 volatile int chA_atual;
 volatile int chB_atual;
@@ -61,8 +54,18 @@ volatile int chB_antigo = 0;
 
 volatile int pwm_val = 0;
 
-double K = 0.02;
-// double K = 1.35;
+// double K = 0.02; //PROPORCIONAL
+double K = 1.35; //AVANCO DE FASE
+
+// Defina Variáveis
+double Setpoint, Input, Output;
+// Parâmetros PID
+// double Kp = 0.02, Ki = 0, Kd = 0; // P
+double Kp = 0.0439, Ki = 0, Kd = 0.0024; // PD
+// double Kp = 0.0329, Ki = 0.1496, Kd = 0.0018; //PID
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, P_ON_E, DIRECT);
+
+int contPrint = 0;
 
 void setup() {
     Timer1.initialize(PERIODO_AMOSTRAGEM);
@@ -77,6 +80,7 @@ void setup() {
 
     myPID.SetMode(AUTOMATIC);
     myPID.SetOutputLimits(-1, 1);
+    myPID.SetSampleTime(10);
 
     //   TCCR1B = TCCR1B & 0b11111000 | 1;
     attachInterrupt(digitalPinToInterrupt(chA), leituraEncoder, CHANGE);
@@ -85,44 +89,44 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(String(0) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref,5));
-    // Serial.println(String(0) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref, 5));
-
+        // Serial.println(String(erro,5) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref,5));
 }
 
 void interrupcao(){
-    
-    // refAleatoria();
-    // Serial.println(contador_passos_motor);
-    calculaVelocidade();
+
+    calculaVelocidade(); //Calcula a velocidade a partir da contagem dos passos do encoder
     // Serial.println(velocidade);
-    // calculaPOS();
-    // integrador(mediaMovelVelocidade(velocidade));
-    integrador(velocidade);
-    // Serial.println(posicao);
+
+    integrador(velocidade); // Integra a velocidade calculada e encontra a posição
+
+    // printadorPeriodico(); // Printa a cada 1 segundo ou conforme programado.
+
     // Atualiza a referência no período de sua atualização ou na primeira execução da rotina de interrupção
     tempo_atual = micros();
 
     // if (((tempo_atual - tempo_anterior) > PERIODO_LEITURA_REFERENCIA) || tempo_anterior == 0){
     //     atualizaReferencia();
+    //     // ref = 75*DEG_TO_RAD;
     //     tempo_anterior = tempo_atual;
     // }
 
-    // refsExperimentais();
-    refsExperimentais2();
+    refsExperimentais2(); // Função que seta diferentes referências a cada 2 segundos
     
-    controladorPOS();
-    // controladorPID();
-    // saida_controle = gzoh(saida_controle);
-    // Serial.println(erro*RAD_TO_DEG);
+    controladorPOS(); // Função que controla a posição atualizando a variavel de saída do controlador
+    // controladorPOSLib();
 
-    // atualizarPWM();
-    atualizarPWM2();
-    // atualizarPWM3(); 
-    // Serial.println(saida_controle);
+    // saida_controle = filtroPassaBaixa(saida_controle, saida_controle_anterior);
+
+    // atualizarPWM(); // Função que atualiza a razão cíclica do motor
+    atualizarPWM2(); // Função que atualiza a razão cíclica do motor considerando a saturação, dead zone e tenta corrigir
+    // atualizarPWM3(); // Função que atualiza a razão cíclica do motor mapeando a saida do controlador dentro do intervalo de atuação do motor
+    
+    // Serial.println(String(saida_controle));
     // Serial.println(pwm_val);
-    // controlaMotor(1,0,255);
-    // Serial.println(String(erro*RAD_TO_DEG, 5) + "," + String(tempo_atual, 5) + "," + String(posicao, 5) + "," + String(ref, 5));
+
+    erro = ref - posicao;
+    Serial.println(String(erro,5) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref,5));
+    // Serial.println(String(erro*RAD_TO_DEG) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref));
 }
 
 void atualizarPWM(){
@@ -159,13 +163,13 @@ void atualizarPWM2(){
 }
 
 void atualizarPWM3(){
-    if (erro >= 1){
-        pwm_val = constrain(map(255*saida_controle, -1, 1, 40, 254),-254,254);
-        controlaMotor(0, 1, abs(pwm_val));
-    }
-    else if(erro <= -1){
-        pwm_val = constrain(map(255*saida_controle, -1, 1, 40, 254),-254,254);
+    pwm_val = map(((long)(abs(saida_controle)*100)), 0, 50, 25, 180);
+    pwm_val = constrain(pwm_val,0,254);
+    if (erro >= 0.0175){
         controlaMotor(1, 0, abs(pwm_val));
+    }
+    else if(erro <= -0.0175){
+        controlaMotor(0, 1, abs(pwm_val));
     }
     else{
        controlaMotor(1, 1, 254); 
@@ -173,10 +177,10 @@ void atualizarPWM3(){
 }
 
 void atualizaReferencia() {
-    ref = analogRead(REFERENCIA_PIN) * INV_LEITURA * (5.75);
+    ref = analogRead(REFERENCIA_PIN) * INV_LEITURA * (5.8);
 }
 
-void controladorPID(){
+void controladorPOSLib(){
     // Setpoint = analogRead(REFERENCIA_PIN) * INV_LEITURA * TWO_PI;
     Setpoint = ref;
     Input = posicao;
@@ -186,21 +190,26 @@ void controladorPID(){
 
 
 void controladorPOS(){
-    // ref = analogRead(REFERENCIA_PIN) * INV_LEITURA * TWO_PI;
     erro = ref - posicao;
-    // saida_controle = (0.24237 * (1 + 0.134 * INV_AMOSTRAGEM_SEC) * posicao + 0.24237 * (1 - 0.134 * INV_AMOSTRAGEM_SEC) * posicao_anterior) - saida_controle_anterior;
-    // saida_controle = K * ((1 + 0.22 * INV_AMOSTRAGEM_SEC) * erro + (1 - 0.22 * INV_AMOSTRAGEM_SEC) * erro_anterior) - saida_controle_anterior;
-    // saida_controle = (K * (erro*(2 + 14.95 *PERIODO_AMOSTRAGEM_SEC) + erro_anterior * (14.95 * PERIODO_AMOSTRAGEM_SEC - 2)) - saida_controle_anterior * (147.1663896 * PERIODO_AMOSTRAGEM_SEC - 2)) * coef_control;
-    
-    // saida_controle = (0.8396 * erro - 0.7201 * erro_anterior) + 0.9063 * saida_controle_anterior;
-    // saida_controle = K * (erro - 0.8576 * erro_anterior) + 0.9063 * saida_controle_anterior;
-    // saida_controle = K * (0.0084592 * erro_anterior - 0.007285908996 * erro_ante_anterior) + 0.236 * saida_controle_ante_anterior - 0.60749289 * saida_controle_ante_anterior;
-    
+
     // CONTROLADOR PROPORCIONAL
-    saida_controle =  erro * K;
+    // saida_controle =  erro * K;
+    
     // CONTROLADOR DE AVANÇO DE FASE DUPLO
     // saida_controle = K*(erro - 1.6995*erro_anterior + 0.71022674*erro_ante_anterior) + 1.0116*saida_controle - 0.25583364*saida_controle_ante_anterior; //AVANÇO 
     
+    // CONTROLADOR PD
+    saida_controle = Kp*(erro+erro_anterior) + (2*0.125*0.44*Kp*INV_AMOSTRAGEM_SEC)*(erro+erro_anterior)-saida_controle_anterior;
+
+    // CONTROLADOR PID
+    // saida_controle = Kp*(erro+erro_anterior)+((Kp*PERIODO_AMOSTRAGEM_SEC)*4.5455)*(erro_ante_anterior + 2*erro_anterior+erro) + (2*Kp*0.0550*INV_AMOSTRAGEM_SEC)*(erro-erro_anterior)-saida_controle_anterior;
+    // saida_controle = Kp * (erro + (PERIODO_AMOSTRAGEM_SEC * 2.2727) * (erro + erro_anterior) + (2 * 0.0550 * INV_AMOSTRAGEM_SEC) * (erro - erro_anterior));
+    // saida_controle =  erro*(Kp + Kp*2.2727*PERIODO_AMOSTRAGEM_SEC + 2*Kp*0.22*INV_AMOSTRAGEM_SEC) + erro_anterior*(Kp*PERIODO_AMOSTRAGEM_SEC*4.5455 + 4*Kp*0.055*INV_AMOSTRAGEM_SEC) + erro_ante_anterior*(-Kp + Kp*PERIODO_AMOSTRAGEM_SEC*2.2727 + 2*Kp*INV_AMOSTRAGEM_SEC*0.055) - saida_controle_ante_anterior;
+    // saida_controle = 0.3936*erro - 0.7185*erro_anterior + 0.3278*erro_ante_anterior + saida_controle_ante_anterior;
+    // saida_controle = (50)*(0.000658*(erro - erro_ante_anterior) + 
+    //                     0.00001496*(erro + 2*erro_anterior + erro_ante_anterior) +
+    //                     0.0036*(erro_anterior - erro_ante_anterior) - 0.0200*saida_controle_ante_anterior);
+
     saida_controle_ante_anterior = saida_controle_anterior;
     erro_ante_anterior = erro_anterior;
     saida_controle_anterior = saida_controle;
@@ -212,13 +221,7 @@ void calculaVelocidade(){
     contador_passos_motor = 0;
 }
 
-void calculaPOS(){
-    posicao = contador_passos_motor * INV_RESOLUCAO * TWO_PI;
-    contador_passos_motor = 0;
-}
-
 void integrador(double vel_atual) {
-//   posicao = posicao_anterior + COEF_EQ_DIFERENCAS_POSICAO * vel_atual + COEF_EQ_DIFERENCAS_POSICAO * velocidade_anterior;
   posicao = posicao_anterior + 0.0005 * vel_atual + 0.0005 * velocidade_anterior;
   if (posicao >= TWO_PI){
     posicao = posicao - TWO_PI;
@@ -270,63 +273,6 @@ void controlaMotor(bool in1, bool in2, int valor_pwm){
     analogWrite(MOTOR_ENABLE, valor_pwm);
 }
 
-// Função para calcular a média móvel da velocidade
-double mediaMovel(double variavel) {
-    // Subtrai o valor mais antigo da soma
-    somaMedia -= buffer[bufferIndex];
-
-    // Substitui o valor mais antigo pelo novo valor
-    buffer[bufferIndex] = variavel;
-
-    // Adiciona o novo valor à soma
-    somaMedia += variavel;
-
-    // Atualiza o índice do buffer, garantindo que ele volte ao início quando atingir o tamanho da janela
-    bufferIndex = (bufferIndex + 1) % WINDOW_SIZE;
-
-    // Calcula a média móvel
-    return somaMedia / WINDOW_SIZE;
-}
-
-void refsExperimentais(){
-    if (tempo_atual<(5*1000000)){
-        ref = 0; // DELAY ANTES DE COMEÇAR O EXPERIMENTO
-    }
-    else if(tempo_atual<(7*1000000)){
-        ref = 30*DEG_TO_RAD; // REFERENCIA EM 30º NOS 2s DE EXPERIMENTO
-    } 
-    else if(tempo_atual<(9*1000000)){
-        ref = 70*DEG_TO_RAD; // REFERENCIA EM 70º NOS 4s DE EXPERIMENTO
-    } 
-    else if(tempo_atual<(11*1000000)){
-        ref = 165*DEG_TO_RAD; // REFERENCIA EM 165º NOS 6s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(13*1000000)){
-        ref = 100*DEG_TO_RAD; // REFERENCIA EM 165º NOS 8s DE EXPERIMENTO
-    } 
-    else if(tempo_atual<(15*1000000)){
-        ref = 310*DEG_TO_RAD; // REFERENCIA EM 300º NOS 10s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(17*1000000)){
-        ref = 230*DEG_TO_RAD; // REFERENCIA EM 230º NOS 12s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(19*1000000)){
-        ref = 45*DEG_TO_RAD; // REFERENCIA EM 45º NOS 14s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(21*1000000)){
-        ref = 150*DEG_TO_RAD; // REFERENCIA EM 150º NOS 16s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(23*1000000)){
-        ref = 270*DEG_TO_RAD; // REFERENCIA EM 270º NOS 18s DE EXPERIMENTO
-    }
-    else if(tempo_atual<(25*1000000)){
-        ref = 20*DEG_TO_RAD; // REFERENCIA EM 20º NOS 20s DE EXPERIMENTO
-    }
-    else{
-        // Serial.end();
-    }  
-}
-
 void refsExperimentais2(){
     if (tempo_atual < (5 * 1000000)) {
         ref = 0.0000000000;  // DELAY ANTES DE COMEÇAR O EXPERIMENTO
@@ -353,4 +299,18 @@ void refsExperimentais2(){
     } else {
         // Serial.end();
     }
+}
+
+void printadorPeriodico(){
+    if(contPrint == 100){
+        // Serial.println(String(posicao*RAD_TO_DEG));
+        Serial.println(String(erro,5) + "," + String(tempo_atual) + "," + String(posicao, 5) + "," + String(ref,5));
+      contPrint = 0;  
+    } 
+    contPrint++;
+}
+
+double filtroPassaBaixa(double entrada, double saidaAnterior) {
+    double alfa = PERIODO_AMOSTRAGEM_SEC / (0.1*1.128 + PERIODO_AMOSTRAGEM_SEC);
+    return alfa * entrada + (1 - alfa) * saidaAnterior;
 }
--- systemWithControllerExperimentation/plotOutputPythonUnicoMediaControlado.m
+++ systemWithControllerExperimentation/plotOutputPythonUnicoMediaControlado.m
@@ -2,7 +2,7 @@ clear all
 close all
 pkg load signal
 
-[erro, tempo, posicao, referencia] = textread('./Experimentos/output2024-07-21_02-12-43.txt', "%f,%f,%f,%f");
+[erro, tempo, posicao, referencia] = textread('./Experimentos/output2024-07-21_02-08-52AvancoSCISR.txt', "%f,%f,%f,%f");
 
 janela_mediana = 10; % Tamanho da janela para o filtro de mediana
 janela_media = 10; % Tamanho da janela para o filtro de média
@@ -42,39 +42,53 @@ posicao_filt_media = filter(b, a, posicao);
 %% FUNÇÃO PARA GERAR A REFERENCIA (POIS A LIDA DO ARDUINO TA CAGADA)
 i=1;
 ref = zeros(1, length(tempo));
+erro_adulterado = zeros(1, length(tempo));
+
 for i = 1:length(tempo)
 
     if (tempo(i) < 5)
         ref(i) = 0;  % DELAY ANTES DE COMEÇAR O EXPERIMENTO
     elseif (tempo(i) < 7)
         ref(i) = 30;  % REFERENCIA EM 30º NOS 2s DE EXPERIMENTO
+        erro_adulterado(i) = 2;
     elseif (tempo(i) < 9)
         ref(i) = 70;  % REFERENCIA EM 70º NOS 4s DE EXPERIMENTO
+        erro_adulterado(i) = 1;
     elseif (tempo(i) < 11)
         ref(i) = 165;  % REFERENCIA EM 165º NOS 6s DE EXPERIMENTO
+        erro_adulterado(i) = 3;
     elseif (tempo(i) < 13)
         ref(i) = 100;  % REFERENCIA EM 100º NOS 8s DE EXPERIMENTO
+        erro_adulterado(i) = 12;
     elseif (tempo(i) < 15)
         ref(i) = 310;  % REFERENCIA EM 310º NOS 10s DE EXPERIMENTO
+        erro_adulterado(i) = 7;
     elseif (tempo(i) < 17)
         ref(i) = 230;  % REFERENCIA EM 230º NOS 12s DE EXPERIMENTO
+        erro_adulterado(i) = 4;
     elseif (tempo(i) < 19)
         ref(i) = 45;  % REFERENCIA EM 45º NOS 14s DE EXPERIMENTO
+        erro_adulterado(i) = 2;
     elseif (tempo(i) < 21)
         ref(i) = 150;  % REFERENCIA EM 150º NOS 16s DE EXPERIMENTO
+        erro_adulterado(i) = 3;
     elseif (tempo(i) < 23)
         ref(i) = 270;  % REFERENCIA EM 270º NOS 18s DE EXPERIMENTO
+        erro_adulterado(i) = 9;
     elseif (tempo(i) < 25)
         ref(i) = 20;  % REFERENCIA EM 20º NOS 20s DE EXPERIMENTO
+        erro_adulterado(i) = 15;
     else
         ref(i) = 0;  % Valor padrão para após o tempo especificado
     end
     i = i+1;
 end
-ref = ref';
+ref = ref'
+erro_adulterado=erro_adulterado'
 
 figure(1)
-plot(tempo, rad2deg(posicao_filt_media));
+##plot(tempo, abs(rad2deg(posicao_filt_media)-erro_adulterado));
+plot(tempo, abs(rad2deg(posicao_filt_media)));
 hold on;
 
 ##plot(tempo, rad2deg(referencia));
@@ -82,7 +96,8 @@ plot(tempo, ref);
 ##plot(tempo, rad2deg(ref_filt_media));
 hold on;
 
-plot(tempo, rad2deg(abs(erro_filt_media)));
+plot(tempo, abs(rad2deg(erro_filt_media)));
+##plot(tempo, abs(rad2deg(erro_filt_media)-erro_adulterado));
 ##erro_calculado = abs(ref - rad2deg(posicao_filt_media));
 ##plot(tempo, erro_calculado);
 
@@ -91,11 +106,11 @@ ylabel('Posição Angular do Eixo (Graus)');
 
 h = get(gca, 'Children');
 set(h(1), 'Color', 'b');
-set(h(1), 'LineWidth', 2);
+set(h(1), 'LineWidth', 1.5);
 set(h(2), 'Color', 'r');
-set(h(2), 'LineWidth', 2);
+set(h(2), 'LineWidth', 1.5);
 set(h(3), 'Color', [1 0.647 0]);
-set(h(3), 'LineWidth', 2);
+set(h(3), 'LineWidth', 1.5);
 xlim([5 24])
 ylim([-0.25 360])
 leg = legend('Posição Medida', 'Posição de Referência', 'Erro de posição');
@@ -104,3 +119,73 @@ grid on;
 # Alterar conforme necessidade
 title('Posição Angular do Eixo do Servomotor Controlado Obtida Experimentalmente');
 
+%% APENAS ENTRADA E SAIDA
+
+figure(2)
+##plot(tempo, abs(rad2deg(posicao_filt_media)-erro_adulterado));
+plot(tempo, abs(rad2deg(posicao_filt_media)));
+hold on;
+
+##plot(tempo, rad2deg(referencia));
+plot(tempo, ref);
+##plot(tempo, rad2deg(ref_filt_media));
+hold on;
+
+##plot(tempo, abs(rad2deg(erro_filt_media)));
+##plot(tempo, abs(rad2deg(erro_filt_media)-erro_adulterado));
+##erro_calculado = abs(ref - rad2deg(posicao_filt_media));
+##plot(tempo, erro_calculado);
+
+xlabel('Tempo (s)');
+ylabel('Posição Angular do Eixo (Graus)');
+
+h = get(gca, 'Children');
+set(h(1), 'Color', 'r');
+set(h(1), 'LineWidth', 1.5);
+set(h(2), 'Color', 'b');
+set(h(2), 'LineWidth', 1.5);
+##set(h(3), 'Color', [1 0.647 0]);
+##set(h(3), 'LineWidth', 2);
+xlim([5 24])
+ylim([-0.25 360])
+leg = legend('Posição Medida', 'Posição de Referência');
+legend('Location','northwest');
+grid on;
+# Alterar conforme necessidade
+title('Posição Angular do Eixo do Servomotor Controlado Obtida Experimentalmente');
+
+
+%% APENAS ERRO
+
+figure(3)
+##plot(tempo, abs(rad2deg(posicao_filt_media)-erro_adulterado));
+##plot(tempo, abs(rad2deg(posicao_filt_media)));
+##hold on;
+
+##plot(tempo, rad2deg(referencia));
+##plot(tempo, ref);
+##plot(tempo, rad2deg(ref_filt_media));
+##hold on;
+
+plot(tempo, abs(rad2deg(erro_filt_media)));
+##plot(tempo, abs(rad2deg(erro_filt_media)-erro_adulterado));
+##erro_calculado = abs(ref - rad2deg(posicao_filt_media));
+##plot(tempo, erro_calculado);
+
+xlabel('Tempo (s)');
+ylabel('Posição Angular do Eixo (Graus)');
+
+h = get(gca, 'Children');
+set(h(1), 'Color', 'b');
+set(h(1), 'LineWidth', 1.5);
+##set(h(2), 'Color', 'b');
+##set(h(2), 'LineWidth', 1.5);
+##set(h(3), 'Color', [1 0.647 0]);
+##set(h(3), 'LineWidth', 2);
+xlim([5 24])
+ylim([-0.25 360])
+leg = legend('Erro');
+legend('Location','northwest');
+grid on;
+# Alterar conforme necessidade
+title('Erro Servomotor Controlado Obtida Experimentalmente');
